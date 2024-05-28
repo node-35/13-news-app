0596f67945dbb5a9c75893bce884f546bd708ab2
andres <andres.david.mm@hotmail.com>
2024-05-27 22:55:23 -0500

    first commit

---
+++ src/tests/user.test.js
@@ -0,0 +1,50 @@
+const app = require('../app');
+const request = require('supertest');
+
+let id;
+
+test('GET /users debe traer todos los usuarios', async () => {
+    const res = await request(app).get('/users');
+    expect(res.status).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+});
+
+test('POST /users debe crear un usuario', async () => {
+    const newUser = {
+        firstName: 'test user',
+        lastName: 'test user',
+        email: 'test@gmail.com',
+        password: 'test1234',
+        gender: 'other',
+    }
+    const res = await request(app).post('/users').send(newUser);
+    id = res.body.id;
+    expect(res.status).toBe(201);
+    expect(res.body.id).toBeDefined();
+    expect(res.body.firstName).toBe(newUser.firstName);
+});
+
+test('POST /users/login debe loggear al usuario', async () => {
+    const credentials = {
+        email: 'test@gmail.com',
+        password: 'test1234',
+    }
+    const res = await request(app).post('/users/login').send(credentials);
+    expect(res.status).toBe(200);
+    expect(res.body.token).toBeDefined();
+    expect(res.body.user.email).toBe(credentials.email);
+});
+
+test('POST /users/login con credenciales incorrectas debe dar error', async () => {
+    const credentials = {
+        email: 'incorrecto@gmail.com',
+        password: 'incorrecto1234',
+    }
+    const res = await request(app).post('/users/login').send(credentials);
+    expect(res.status).toBe(401);
+});
+
+test('DELETE /users/:id debe eliminar un usuario', async () => {
+    const res = await request(app).delete(`/users/${id}`);
+    expect(res.status).toBe(204);
+});
